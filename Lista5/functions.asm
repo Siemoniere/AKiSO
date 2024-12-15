3cf97313dc4f1de8f0cfa3840756013db328d781
Szymon Hładyszewski <161628801+Siemoniere@users.noreply.github.com>
2024-12-15 14:01:31 +0100

    Add files via upload

--- Lista5/functions.asm
+++ Lista5/functions.asm
@@ -32,7 +32,6 @@ printLoop:
     pop     eax             ; restore eax from the value we pushed onto the stack at the start
     ret
  
- 
 ;------------------------------------------
 ; void iprintLF(Integer number)
 ; Integer printing function with linefeed (itoa)
@@ -47,7 +46,6 @@ iprintLF:
     pop     eax             ; remove our linefeed character from the stack
     pop     eax             ; restore the original value of eax before our function was called
     ret
- 
  
 ;------------------------------------------
 ; int slen(String message)
@@ -68,7 +66,6 @@ finished:
     sub     eax, ebx       ; oblicz długość (adres końca - początek)
     pop     ebx
     ret
- 
  
 ;------------------------------------------
 ; void sprint(String message)
@@ -93,7 +90,6 @@ sprint:
     pop     edx
     ret
  
- 
 ;------------------------------------------
 ; void sprintLF(String message)
 ; String printing with line feed function
@@ -109,7 +105,6 @@ sprintLF:
     pop     eax
     ret
  
- 
 ;------------------------------------------
 ; void exit()
 ; Exit program and restore resources
@@ -118,42 +113,36 @@ quit:
     mov     eax, 1
     int     80h
     ret
-;------------------------------------------
-; int atoi(Integer number)
-; Ascii to integer function (atoi)
-atoi:
-    push    ebx             ; preserve ebx on the stack to be restored after function runs
-    push    ecx             ; preserve ecx on the stack to be restored after function runs
-    push    edx             ; preserve edx on the stack to be restored after function runs
-    push    esi             ; preserve esi on the stack to be restored after function runs
-    mov     esi, eax        ; move pointer in eax into esi (our number to convert)
-    mov     eax, 0          ; initialise eax with decimal value 0
-    mov     ecx, 0          ; initialise ecx with decimal value 0
- 
-.multiplyLoop:
-    xor     ebx, ebx        ; resets both lower and uppper bytes of ebx to be 0
-    mov     bl, [esi+ecx]   ; move a single byte into ebx register's lower half
-    cmp     bl, 48          ; compare ebx register's lower half value against ascii value 48 (char value 0)
-    jl      .finished       ; jump if less than to label finished
-    cmp     bl, 57          ; compare ebx register's lower half value against ascii value 57 (char value 9)
-    jg      .finished       ; jump if greater than to label finished
- 
-    sub     bl, 48          ; convert ebx register's lower half to decimal representation of ascii value
-    add     eax, ebx        ; add ebx to our integer value in eax
-    mov     ebx, 10         ; move decimal value 10 into ebx
-    mul     ebx             ; multiply eax by ebx to get place value
-    inc     ecx             ; increment ecx (our counter register)
-    jmp     .multiplyLoop   ; continue multiply loop
- 
-.finished:
-    cmp     ecx, 0          ; compare ecx register's value against decimal 0 (our counter register)
-    je      .restore        ; jump if equal to 0 (no integer arguments were passed to atoi)
-    mov     ebx, 10         ; move decimal value 10 into ebx
-    div     ebx             ; divide eax by value in ebx (in this case 10)
- 
-.restore:
-    pop     esi             ; restore esi from the value we pushed onto the stack at the start
-    pop     edx             ; restore edx from the value we pushed onto the stack at the start
-    pop     ecx             ; restore ecx from the value we pushed onto the stack at the start
-    pop     ebx             ; restore ebx from the value we pushed onto the stack at the start
+
+;-------------------------------------------
+;void is_first(Integer number)
+
+is_first:
+    push	eax
+    push	ecx
+    push	ebx
+    push	esi
+    mov		esi, eax
+    mov		ebx, 2 ;zaczynamy iterowac od 2 do liczby spawdzanej
+
+check:
+    cmp		ebx, esi
+    jae		print
+    xor		edx, edx
+    mov		eax, esi
+    div		ebx
+    test	edx, edx ;sprawdza czy ecx!=0
+    jz		end
+    inc		ebx
+    jmp		check
+
+print:
+    mov		eax, esi
+    call	iprintLF
+
+end:
+    pop		esi
+    pop		ebx
+    pop		ecx
+    pop		eax
     ret
--- Lista5/zadanie1.asm
+++ Lista5/zadanie1.asm
@@ -1,53 +1,53 @@
-%include        'functions.asm'
+%include	'functions.asm'
 SECTION .data
-msg1    db      'Podaj ciag cyfr: ', 0h
-msg2    db      'Wynik to: ', 0h
+msg1	db	'Podaj ciag cyfr: ', 0h
+msg2	db	'Wynik to: ', 0h
 
 SECTION .bss
-sinput:         resb    255
+sinput:		resb	255
 
 SECTION .text
 global _start
 _start:
     ;printujemy polecenie
-    mov         eax, msg1
-    call        sprint
+    mov		eax, msg1
+    call	sprint
 
     ;wczytujemy dane ktore przechowujemy w sinput
-    mov         edx, 255
-    mov         ecx, sinput
-    mov         ebx, 0
-    mov         eax, 3
-    int         80h
+    mov		edx, 255
+    mov		ecx, sinput
+    mov		ebx, 0
+    mov		eax, 3
+    int		80h
 
     ; Dodajemy null-terminator na końcu wczytanego ciągu
-    mov         byte [sinput + eax - 1], 0
+    mov		byte [sinput + eax - 1], 0
 
     ;pod edx podstawiamy liczbe cyfr i zerujemy ebx
-    mov         eax, sinput
-    call        slen
-    mov         edx, eax
-    xor         ebx, ebx
+    mov		eax, sinput
+    call	slen
+    mov		edx, eax
+    xor		ebx, ebx
 
 loop:
-    cmp         edx, 0 ;dopóki długosc nie wynosi zero to dalej dodajemy poszczegolne ctyfry
-    jz          finish
+    cmp		edx, 0 ;dopóki długosc nie wynosi zero to dalej dodajemy poszczegolne ctyfry
+    jz		finish
 
-    mov         ecx, sinput
-    dec         edx
-    add         ecx, edx
+    mov		ecx, sinput
+    dec		edx
+    add		ecx, edx
 
-    movzx       eax, byte [ecx]
-    sub         eax, '0'
-    add         ebx, eax
+    movzx	eax, byte [ecx]
+    sub		eax, '0'
+    add		ebx, eax
 
-    jmp         loop
+    jmp		loop
 
 ;printujemy wynik
 finish:
-    mov         eax, msg2
-    call        sprint
-    mov         eax, ebx
-    call        iprintLF
+    mov		eax, msg2
+    call	sprint
+    mov		eax, ebx
+    call	iprintLF
     ;konczymy
-    call        quit
+    call	quit
